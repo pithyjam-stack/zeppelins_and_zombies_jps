7c06e6ad11ad85202f859800f9650cd718f47659
Tyler Pederson <tyler.lee.pederson@gmail.com>
2026-05-27 22:25:53 -0700

    Player faces where mouse is pointing
    This also sets up for the camera to be able to 'select' objects it hovers over.

---
+++ scenes/player/ray_camera.gd
@@ -0,0 +1,28 @@
+extends Camera3D
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+const RAY_DISTANTCE = 100
+
+var shape : MeshInstance3D
+var player : Player
+
+func _ready() -> void:
+	#shape = MeshInstance3D.new()
+	#shape.mesh = SphereMesh.new()
+	#shape.name = "DEBUG VISUAL AIM"
+	#get_tree().root.add_child.call_deferred(shape)
+	player = get_tree().get_first_node_in_group("Player")
+
+func _process(delta: float) -> void:
+	var mouse_position : Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * RAY_DISTANTCE
+	ray_cast_3d.force_raycast_update()
+	
+	var collider := ray_cast_3d.get_collider()
+	if ray_cast_3d.is_colliding():
+		if !collider.is_in_group("Playerf"):
+			#shape.global_position = ray_cast_3d.get_collision_point()
+			var direction : Vector3 = ray_cast_3d.get_collision_point() - player.global_position
+			player._rotate_to_direction(direction, delta)
+	
